f8b9bac6b9fdfc8321bdd83acd1bdcf3481b2df3
vista521 <kapiservice@sina.com>
2015-04-18 16:28:36 +0800

    Initial Commit

---
+++ SyrahLibraryMobileTest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SyrahLibraryMobileTest2
+//
+//  Created by Jerry Lee on 15/4/18.
+//  Copyright (c) 2015年 Jerry's Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SyrahLibraryMobileTest2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SyrahLibraryMobileTest2
+//
+//  Created by Jerry Lee on 15/4/18.
+//  Copyright (c) 2015年 Jerry's Studio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
